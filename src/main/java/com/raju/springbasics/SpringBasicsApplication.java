410d9fd3f748826a7778be610f9d7cba9650c742
Sai Sarath <saisarathchandrap@gmail.com>
2022-03-22 13:00:28 +0530

    GET API Implemented

--- src/main/java/com/raju/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/raju/springbasics/SpringBasicsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("SpringBasicsApplication.main");
 		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
 
